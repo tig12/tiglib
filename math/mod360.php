dc13abee3a79ca3d72afb7e9c68aea7df7e738b1
Thierry <tigraff@gmail.com>
2021-05-15 03:16:02 +0200

    Add math/mod360 + unit test

---
+++ math/mod360.php
@@ -0,0 +1,35 @@
+<?php
+/******************************************************************************
+
+
+    WARNING : works for positive numbers, but negative numbers must be tested and debugged
+    (see unit test with -360)
+
+    @license    GPL
+    @history    2021-02-28 10:18:57+01:00, Thierry Graff : Creation from old code
+********************************************************************************/
+
+namespace tiglib\math;
+
+class mod360 {
+    
+    //***************************************************
+    /**
+        Returns a number modulo 360 (between 0 and 360).
+        $nb can be integer or decimal, positive or negative
+    **/
+    public static function compute($nb){
+        if($nb >= 0){
+            $dec = $nb - floor($nb);
+            return $nb%360 + $dec;
+        }
+        else{
+            $dec = $nb - floor($nb);
+            if($dec != 0){
+                $dec -= 1;
+            }
+            return $nb%360 + $dec + 360;
+        }
+    }
+
+} // end class
---
+++ math/mod360Test.php
@@ -0,0 +1,32 @@
+<?php
+/******************************************************************************
+
+    @license    GPL
+    @history    2021-02-28 10:32:53+01:00, Thierry Graff : Creation
+********************************************************************************/
+
+namespace tiglib\math;
+
+use PHPUnit\Framework\TestCase;
+use \tiglib\math\mod360;
+
+
+class mod360Test extends TestCase {
+
+    public function test0(){
+        $this->assertEquals( 0, mod360::compute(0) );
+        $this->assertEquals( 0, mod360::compute(360) );
+//        $this->assertEquals( 0, mod360::compute(-360) );
+    }
+    
+    public function testPositive(){
+        $this->assertEquals( 80, mod360::compute(80) );
+        $this->assertEquals( 1, mod360::compute(361) );
+    }
+    
+    public function testNegative(){
+        $this->assertEquals( 120, mod360::compute(-240) );
+        $this->assertEquals( 1, mod360::compute(-359) );
+    }
+    
+} // end class
