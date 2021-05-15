3a06f415ba7b203509f3b58849ab22cf5c5b7d21
Thierry <tigraff@gmail.com>
2021-05-15 03:17:45 +0200

    Add filesystem/rrmdir

---
+++ filesystem/rrmdir.php
@@ -0,0 +1,23 @@
+<?php
+/******************************************************************************
+    Recursively delete a directory
+    Adaptation of example code found at
+    https://www.php.net/manual/en/function.rmdir.php
+    
+    @license    GPL
+    @history    2021-02-08 17:14:42+01:00, Thierry Graff : Creation
+********************************************************************************/
+
+namespace tiglib\filesystem;
+
+class rrmdir{
+    
+   public static function execute($dir){
+        $files = array_diff(scandir($dir), array('.','..'));
+        foreach ($files as $file) {
+            (is_dir("$dir/$file")) ? self::execute("$dir/$file") : unlink("$dir/$file");
+        }
+        return rmdir($dir);
+    }
+    
+}
