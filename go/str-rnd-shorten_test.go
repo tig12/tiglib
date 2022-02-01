871565197dec1b030b3e11965593c65035faf1b3
Thierry <tigraff@gmail.com>
2022-02-01 01:12:39 +0100

    Add go/StrRandomShorten

---
+++ go/str-rnd-shorten_test.go
@@ -0,0 +1,10 @@
+package tiglib
+
+import "testing"
+
+func TestStrRandomShorten(t *testing.T) {
+	str := "6022489e0260daa8ef4af916cf456f07"
+	if StrRandomShorten(str, 8)  != "589a89f4" {
+		t.Error("StrRandomShorten(6022489e0260daa8ef4af916cf456f07) should be equal to 589a89f4")
+	}
+}
