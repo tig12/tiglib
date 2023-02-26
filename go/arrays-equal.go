e520e2f709fb8c487c987029037a2b301808a4b3
Thierry Graff <tigraff@gmail.com>
2023-02-26 01:43:19 +0100

    Generic version of go/arrays-equal

--- go/arrays-equal.go
+++ go/arrays-equal.go
@@ -7,6 +7,23 @@
 **/
 package tiglib
 
+// Tests if two slices of comparable types contain the same elements
+// Generic function
+//func ArraysEqual(a1, a2 []ArrayElement) bool {
+func ArraysEqual[T comparable](a1, a2 []T) bool {
+	if len(a1) != len(a2) {
+		return false
+	}
+	for i := range a1 {
+		if a1[i] != a2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// TODO Remove following code and corresponding tests when all client gode is go >= 1.18
+
 // Tests if two slices of strings contain the same elements
 func ArraysEqualString(a1, a2 []string) bool {
 	if len(a1) != len(a2) {
--- go/arrays-equal_test.go
+++ go/arrays-equal_test.go
@@ -6,26 +6,26 @@ package tiglib
 
 import "testing"
 
-func TestArraysEqualString(t *testing.T) {
+func TestArraysEqual(t *testing.T) {
 	a1 := []string{"a", "b", "c"}
 	a2 := []string{"a", "b", "c"}
 	a3 := []string{"a", "c", "b"}
 	a4 := []string{"a", "b", "c", "d"}
 	a5 := []string{}
 	a6 := []string{}
-	if !ArraysEqualString(a1, a2) {
+	if !ArraysEqual(a1, a2) {
 		t.Error(`{"a", "b", "c"} and {"a", "b", "c"} should be equal`)
 	}
-	if ArraysEqualString(a1, a3) {
+	if ArraysEqual(a1, a3) {
 		t.Error(`{"a", "b", "c"} and {"a", "c", "b"} should not be equal`)
 	}
-	if ArraysEqualString(a1, a4) {
+	if ArraysEqual(a1, a4) {
 		t.Error(`{"a", "b", "c"} and {"a", "b", "c", "d"} should not be equal`)
 	}
-	if ArraysEqualString(a1, a5) {
+	if ArraysEqual(a1, a5) {
 		t.Error(`{"a", "b", "c"} and {} should not be equal`)
 	}
-	if !ArraysEqualString(a5, a6) {
+	if !ArraysEqual(a5, a6) {
 		t.Error(`{} and {} should be equal`)
 	}
 }
@@ -37,19 +37,19 @@ func TestArraysEqualInt(t *testing.T) {
 	a4 := []int{1, 2, 3, 4}
 	a5 := []int{}
 	a6 := []int{}
-	if !ArraysEqualInt(a1, a2) {
+	if !ArraysEqual(a1, a2) {
 		t.Error(`{1, 2, 3} and {1, 2, 3} should be equal`)
 	}
-	if ArraysEqualInt(a1, a3) {
+	if ArraysEqual(a1, a3) {
 		t.Error(`{1, 2, 3} and {1, 3, 2} should not be equal`)
 	}
-	if ArraysEqualInt(a1, a4) {
+	if ArraysEqual(a1, a4) {
 		t.Error(`{1, 2, 3} and {1, 2, 3, 4} should not be equal`)
 	}
-	if ArraysEqualInt(a1, a5) {
+	if ArraysEqual(a1, a5) {
 		t.Error(`{1, 2, 3} and {} should not be equal`)
 	}
-	if !ArraysEqualInt(a5, a6) {
+	if !ArraysEqual(a5, a6) {
 		t.Error(`{} and {} should be equal`)
 	}
 }
