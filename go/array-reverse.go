0472915a93d0e59bb298a62f3e949dffaea35407
Thierry Graff <tigraff@gmail.com>
2023-05-04 15:13:42 +0200

    Add go/array-reverse

---
+++ go/array-reverse.go
@@ -0,0 +1,14 @@
+/**
+    @copyright  Thierry Graff
+    @license    GPL - conforms to file LICENCE located in root directory of current repository.
+**/
+package tiglib
+
+// ArrayReverse reverses a slice in-place
+// From https://stackoverflow.com/questions/13190836/is-there-a-way-to-iterate-over-a-slice-in-reverse-in-go
+// (solution of Ivan Voras)
+func ArrayReverse[S ~[]E, E any](s S) {
+    for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+        s[i], s[j] = s[j], s[i]
+    }
+}
