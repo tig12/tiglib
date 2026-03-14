cc7f103905197aff0793c192ec650c6cfdba86fa
Thierry Graff <tigraff@gmail.com>
2026-03-14 08:47:53 +0100

    Add a parameter to php/time/daysOfYear.php

--- php/time/daysOfYear.php
+++ php/time/daysOfYear.php
@@ -9,17 +9,24 @@ namespace tiglib\time;
 class daysOfYear {
     
     /**
-        Computes an array of days YYYY-MM-DD of a given year.
+        Computes an array of days of a given year, formatted YYYY-MM-DD or MM-DD.
         @param  $year   ex: 1985
-        @return         ex: ['1985-01-01' ... '1985-12-31']
+        @param  $YYYYMMDD   if true, the result is formatted YYYY-MM-DD
+                            if false, the result is formatted MM-DD
+        @return     ex: ['1985-01-01' ... '1985-12-31']
     **/
-    public static function compute(string $year): array {
+    public static function compute(string $year, bool $YYYYMMDD=true): array {
         $res = [];
         $start = new \DateTime("$year-01-01");
         $end   = (new \DateTime("$year-12-31"))->modify('+1 day');
         $period = new \DatePeriod($start, new \DateInterval('P1D'), $end);
         foreach ($period as $date) {
-            $res[] = $date->format('Y-m-d');
+            if($YYYYMMDD){
+                $res[] = $date->format('Y-m-d');
+            }
+            else{
+                $res[] = $date->format('m-d');
+            }
         }
         return $res;
     }
